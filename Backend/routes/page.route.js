dfc2da139a3fdd88844e3a1b63efb3931ce8000f
shashwat <work.shashwatojha@gmail.com>
2025-11-03 10:21:37 +0530

    search function in backend1

--- Backend/routes/page.route.js
+++ Backend/routes/page.route.js
@@ -2,12 +2,13 @@ import express from "express";
 import {
   individualProduct,
   productData,
+  searchData,
 } from "../controllers/page.controller.js";
 
 const router = express.Router();
 
 router.get("/", productData);
 router.get("/showProduct/:id", individualProduct);
-router.get("/search/:searchParam", individualProduct);
+router.get("/search/:searchParam", searchData);
 
 export default router;
