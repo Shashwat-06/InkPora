40acdb2ee3e5c905ce56c5bf13308734ca7a48aa
shashwat <work.shashwatojha@gmail.com>
2026-02-06 16:38:11 +0530

    fixed some ui problems

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -1,11 +1,30 @@
+// import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import { Outlet } from "react-router-dom";
+
+// function Layout() {
+//   return (
+//     <div className="min-h-screen flex flex-col bg-inkpora-bg">
+//       <Header />
+//       <main className="flex-1 bg-inkporabg">
+//         <Outlet />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default Layout;
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 function Layout() {
   return (
     <div className="min-h-screen flex flex-col bg-inkpora-bg">
       <Header />
+      <ScrollToTop />
       <main className="flex-1 bg-inkporabg">
         <Outlet />
       </main>
