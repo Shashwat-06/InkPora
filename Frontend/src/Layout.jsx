709f97e531e3a662fcacba1cd1e6ce286cf58e3b
shashwat <work.shashwatojha@gmail.com>
2026-02-06 16:46:07 +0530

    fixed some ui

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -18,7 +18,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-import ScrollToTop from "./components/ScrollToTop";
+import ScrollToTop from "./components/scrollToTop";
 
 function Layout() {
   return (
